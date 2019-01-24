4b7cea24256473d79309758c7528f8e6a9b9f4dc
Minh Nguyen <minhnd081293@gmail.com>
2019-01-24 23:41:24 +0700

    Updates

---
+++ precache-manifest.1f2c1e3781697cdbd0e01a840ee437f0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/new-facebook/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "edfd61e3a5d911793981",
+    "url": "/new-facebook/static/css/main.4b31150f.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/new-facebook/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c6e5a77ba48c15f72ae1",
+    "url": "/new-facebook/static/js/1.c6e5a77b.chunk.js"
+  },
+  {
+    "revision": "89bc2fa85383364d46da",
+    "url": "/new-facebook/static/js/runtime~main.89bc2fa8.js"
+  },
+  {
+    "revision": "edfd61e3a5d911793981",
+    "url": "/new-facebook/static/js/main.edfd61e3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/new-facebook/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/new-facebook/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/new-facebook/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "c6e5a77ba48c15f72ae1",
+    "url": "/new-facebook/static/css/1.4f8e5b96.chunk.css"
+  },
+  {
+    "revision": "952bdc49e0157afa5a1830a7fe965ed6",
+    "url": "/new-facebook/index.html"
+  }
+];
